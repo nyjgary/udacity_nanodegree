22a87691beb655dbdcb5720e63e6a9b9c90d4e2e
Gary Ng <nyjgary@gmail.com>
2016-04-23 00:06:48 -0400

    submission v1

--- Enron/poi_id.py
+++ Enron/poi_id.py
@@ -1,7 +1,10 @@
-#!/usr/bin/python
+### Note: Most of the work was done in iPython notebook (see attachment). 
+### Included here are the bare minimum code required to test final classifier. 
 
 import sys
 import pickle
+import numpy as np 
+import pandas as pd 
 sys.path.append("../tools/")
 
 from feature_format import featureFormat, targetFeatureSplit
@@ -10,17 +13,71 @@ from tester import dump_classifier_and_data
 ### Task 1: Select what features you'll use.
 ### features_list is a list of strings, each of which is a feature name.
 ### The first feature must be "poi".
-features_list = ['poi','salary'] # You will need to use more features
-
+features_list = ['other','expenses', 'bonus', 'deferred_income',
+                 'from_poi_to_this_person', 'salary',
+                 'from_this_person_to_poi_pct'] # You will need to use more features
 ### Load the dictionary containing the dataset
 with open("final_project_dataset.pkl", "r") as data_file:
     data_dict = pickle.load(data_file)
 
 ### Task 2: Remove outliers
-### Task 3: Create new feature(s)
-### Store to my_dataset for easy export below.
-my_dataset = data_dict
+# 'BHATNAGAR SANJAY' was an outlier that turns out to be due to data entry error
+# The data point was kept but values were updated per the PDF 
+del data_dict['TOTAL']
+data_dict['BHATNAGAR SANJAY']['deferral_payments'] = 0
+data_dict['BHATNAGAR SANJAY']['total_payments'] = 137864
+data_dict['BHATNAGAR SANJAY']['exercised_stock_options'] = 15456290
+data_dict['BHATNAGAR SANJAY']['restricted_stock'] = 2604490
+data_dict['BHATNAGAR SANJAY']['restricted_stock_deferred'] = -2604490
+data_dict['BHATNAGAR SANJAY']['total_stock_value'] = 15456290
+data_dict['BHATNAGAR SANJAY']['expenses'] = 137864
+data_dict['BHATNAGAR SANJAY']['other'] = 0
+data_dict['BHATNAGAR SANJAY']['director_fees'] = 0
 
+
+### Task 3a: Create new feature(s)
+
+for person in data_dict: 
+    try: 
+        data_dict[person]['from_this_person_to_poi_pct'] = data_dict[person]['from_this_person_to_poi'] / data_dict[person]['from_messages'] * 100
+    except TypeError:
+        data_dict[person]['from_this_person_to_poi_pct'] = 'NaN'
+    try: 
+        data_dict[person]['from_poi_to_this_person_pct'] = data_dict[person]['from_poi_to_this_person'] / data_dict[person]['to_messages'] * 100
+    except TypeError: 
+        data_dict[person]['from_poi_to_this_person_pct'] = 'NaN'
+    try: 
+        data_dict[person]['shared_receipt_with_poi_pct'] = data_dict[person]['shared_receipt_with_poi'] / data_dict[person]['to_messages'] * 100
+    except TypeError: 
+        data_dict[person]['shared_receipt_with_poi_pct'] = 'NaN'
+        
+
+### Task 3b: Fill in missing values 
+# Instead of using featureFormat to imput values, I am doing it on my own here 
+
+# Convert to pd dataframe for easy computation of medians 
+df = pd.DataFrame.from_dict(data_dict,orient='index')
+df.replace("NaN", np.nan, inplace=True) # properly encode null values 
+
+# Impute missing email information using medians 
+features_imputed = ['to_messages', 'from_messages', 'from_this_person_to_poi', 'from_poi_to_this_person', 
+                    'shared_receipt_with_poi', 'from_this_person_to_poi_pct', 'from_poi_to_this_person_pct',
+                    'shared_receipt_with_poi_pct', 'salary', 'bonus']
+for variable in features_imputed: 
+    df[variable] = np.where(np.isnan(df[variable]), 
+                            np.median(df[~np.isnan(df[variable])][variable]), df[variable])
+    
+# Replace all NaN's in non-Salary/Bonus financial variables 
+features_zeroed = ['long_term_incentive', 'deferred_income', 'deferral_payments', 'other', 
+                   'expenses', 'director_fees','total_payments', 'exercised_stock_options', 
+                   'restricted_stock', 'restricted_stock_deferred', 'total_stock_value'] 
+for variable in features_zeroed: 
+    df[variable] = np.where(np.isnan(df[variable]),0,df[variable])
+
+
+### Store to my_dataset for easy export below.
+
+my_dataset = df.to_dict(orient='index') # convert back to dict 
 ### Extract features and labels from dataset for local testing
 data = featureFormat(my_dataset, features_list, sort_keys = True)
 labels, features = targetFeatureSplit(data)
@@ -31,10 +88,6 @@ labels, features = targetFeatureSplit(data)
 ### you'll need to use Pipelines. For more info:
 ### http://scikit-learn.org/stable/modules/pipeline.html
 
-# Provided to give you a starting point. Try a variety of classifiers.
-from sklearn.naive_bayes import GaussianNB
-clf = GaussianNB()
-
 ### Task 5: Tune your classifier to achieve better than .3 precision and recall 
 ### using our testing script. Check the tester.py script in the final project
 ### folder for details on the evaluation method, especially the test_classifier
@@ -42,10 +95,18 @@ clf = GaussianNB()
 ### stratified shuffle split cross validation. For more info: 
 ### http://scikit-learn.org/stable/modules/generated/sklearn.cross_validation.StratifiedShuffleSplit.html
 
-# Example starting point. Try investigating other evaluation techniques!
-from sklearn.cross_validation import train_test_split
-features_train, features_test, labels_train, labels_test = \
-    train_test_split(features, labels, test_size=0.3, random_state=42)
+# --- PLEASE SEE PYTHON NOTEBOOK FOR DETAILS, ONLY FINAL CLF INCLUDED BELOW ---- 
+
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.pipeline import make_pipeline
+
+clf = make_pipeline(MinMaxScaler(), 
+                    DecisionTreeClassifier(criterion = 'entropy',
+                                           max_depth = 6,
+                                           max_features = None, 
+                                           min_samples_split = 2,
+                                           random_state = 42))
 
 ### Task 6: Dump your classifier, dataset, and features_list so anyone can
 ### check your results. You do not need to change anything below, but make sure
